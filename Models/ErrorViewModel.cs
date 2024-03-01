52dbc4029b2899912bda7c9cd01c8adcb218f455
MalSyaf <syafqz@gmail.com>
2024-03-01 10:12:21 +0800

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace web_links.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
